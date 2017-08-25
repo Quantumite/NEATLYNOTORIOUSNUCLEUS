59955a1ea181cdb10d6eb84060e1139b814c351c
root <root@localhost.localdomain>
2017-08-25 00:14:21 -0400

    Added encryption

--- nnn.py
+++ nnn.py
@@ -2,6 +2,47 @@
 
 from PIL import Image
 import argparse, sys, os, tempfile, math, struct
+from Crypto.Cipher import AES
+
+IV = '\xDE\xAD\xBE\xEF'*4
+DEFAULT_KEY = '\x00'*16
+
+def addPadding(length):
+	padding = {0: "\x00"*16,
+		1:"\x01"*1,
+		2:"\x02"*2,
+		3:"\x03"*3,
+		4:"\x04"*4,
+		5:"\x05"*5,
+		6:"\x06"*6,
+		7:"\x07"*7,
+		8:"\x08"*8,
+		9:"\x09"*9,
+		10:"\x0A"*10,
+		11:"\x0B"*11,
+		12:"\x0C"*12,
+		13:"\x0D"*13,
+		14:"\x0E"*14,
+		15:"\x0F"*15}	
+
+	if length < 0:
+		return -1
+	elif length >= 16:
+		return -2
+	else:
+		return padding[length]
+
+def removePadding(message):
+	k = message[-1]
+	v = int(struct.unpack("<L", k+'\x00'*3)[0])
+	if v < 0:
+		return -1
+	elif v >= 16:
+		return -2
+	else:
+		if v == 0:
+			v = 16
+		return message[:-1*v]
 
 def readFileSize(myFile, size, start=0, verbose=False):
 	temp=''
@@ -26,13 +67,13 @@ def readFileSize(myFile, size, start=0, verbose=False):
 		print "[readFileSize][DEBUG] First chunk of f.readlines: " + str(temp[0])
 	return ''.join(temp)
 
-def encodeText(text, outfile, width, height, imageType, verbose=False):
+def encodeText(text, outfile, width, height, imageType, verbose=False, encrypt=False, key=DEFAULT_KEY):
 	t = "/tmp/tmp.tmp"
 	with open(t, 'wb') as f:
 		f.write(text)
 	if verbose:
 		print "[encodeText][DEBUG] Created temporary file: " + str(t)
-	encode(t, outfile, width, height, imageType, verbose)
+	encode(t, outfile, width, height, imageType, verbose, encrypt, key)
 	try:
 		if verbose:
 			print "[encodeText][DEBUG] Attempting to remove temporary file."
@@ -43,7 +84,7 @@ def encodeText(text, outfile, width, height, imageType, verbose=False):
 	if verbose:
 		print "[encodeText][DEBUG] File was encoded and temporary file removed, if existed."
 
-def encode(infile, outfile, width, height, imageType, verbose=False):
+def encode(infile, outfile, width, height, imageType, verbose=False, encrypt=False, key=DEFAULT_KEY):
 	#Process arguments
 	if infile is None:
 		print "[!!] No input file specified!"
@@ -72,6 +113,24 @@ def encode(infile, outfile, width, height, imageType, verbose=False):
 	if verbose:
 		print "[encode][DEBUG] Finished reading in file, first 10 bytes: " + str(temp[:10])
 
+	if encrypt:
+		global IV
+		if verbose:
+			print "[encode][DEBUG] Length of data: "+str(len(temp))
+			print "[encode][DEBUG] Length of data%16: "+str(len(temp)%16)
+		padding = addPadding(16 - (len(temp) % 16))
+		
+		if padding == -1:
+			print "The length of the file appeared negative...THIS SHOULD NOT HAPPEN!"
+			quit(4)
+		if padding == -2:
+			print "The length of the file mod 16 was larger than 16...THIS SHOULD NOT HAPPEN!"
+			quit(5)
+		obj = AES.new(key, AES.MODE_CBC, IV)
+		temp = obj.encrypt(temp+padding)
+		if verbose:
+			print "[encode][DEBUG] Finished Encrypting with key: " + str(key)
+			print "[encode][DEBUG] First 10 bytes of encrypted data: " + str(temp[:10])	
 	count = 0
 	tempLen = len(temp)
 	lenArray = struct.unpack('4B', struct.pack('>I', tempLen))
@@ -91,7 +150,7 @@ def encode(infile, outfile, width, height, imageType, verbose=False):
 	#write pixels to image
 	img.save(outfile,imageType)
 
-def decode(infile, outfile, imageType, verbose=False):
+def decode(infile, outfile, imageType, verbose=False, decrypt=False, key='key'):
 	
 	if infile is None:
 		print "[!!] No input file specified!"
@@ -122,7 +181,21 @@ def decode(infile, outfile, imageType, verbose=False):
 				t=pixels[i,j][0]
 				temp += chr(t)
 				count += 1
-
+	
+	if decrypt:
+		global IV
+		obj = AES.new(key, AES.MODE_CBC, IV)
+		temp = obj.decrypt(temp)
+		temp = removePadding(temp)
+		if temp == -1:
+			print "The last byte in the data is negative, with correct padding this should not have happened!"
+			quit(6)
+		if temp == -2:
+			print "The last byte in the data is larger than 16, with correct padding this should not have happened!"
+			quit(7)
+		if verbose:
+			print "[decode][DEBUG] Finished decrypting data with key: " + str(key)
+			print "[decode][DEBUG] First ten bytes of decrypted data: " + str(temp[:10])
 	if verbose:
 		print "[decode][DEBUG] Finished pulling data from image. First 10 chars: " + str(temp[:10])
 		print "[decode][DEBUG] Output file is: " + str(outfile)	
@@ -134,7 +207,7 @@ def decode(infile, outfile, imageType, verbose=False):
 
 def main():
 	#ArgParse section
-	parser = argparse.ArgumentParser(description='Save your data as an image to be uploaded to Amazon!')
+	parser = argparse.ArgumentParser(description='Save your data as an image!')
 	parser.add_argument('--infile', action='store', nargs='?', type=str, default=sys.stdin,help='Input file name.')
 	parser.add_argument('--outfile', action='store', nargs='?', type=str, default=sys.stdout, help="Output file name.")
 	parser.add_argument('--img-width', action='store', nargs='?', type=int, default=100, help="Specify an image width, default=100.")
@@ -145,6 +218,9 @@ def main():
 	parser.add_argument('--split', action='store_true', help="Use this flag when the file is larger that 1GB. The script will not work if the file is larger than 1GB and this flag is not used.")
 	parser.add_argument('--split-size', action='store', nargs='?', type=int, default=1024, help='Use this to specify what size you would like to split your file into. Default 1024 bytes.')
 	parser.add_argument('-v', '--verbose', action='store_true', help='Use to display debugging information.')
+	parser.add_argument('--encrypt', action='store_true', help="Include this flag to encrypt data before it is placed in the image.")
+	parser.add_argument('--decrypt', action='store_true', help="Include this flag to decrypt data before writing to the output file.")
+	parser.add_argument('--key', action='store', nargs='?', type=str, default='key', help="This flag, combined with the --encrypt or --decrypt flag will encrypt or decrypt the data using AES-CBC mode and this key. The key must be 16, 24 or 32 bytes in length.")
 	args = parser.parse_args()
 
 	#Determining which image type to use
@@ -179,20 +255,20 @@ def main():
 				print "[DEBUG] Input File Name: " + str(args.infile)
 				print "[DEBUG] Starting to read at byte: " + str(i*size)
 				print "[DEBUG] Output file name(s): " + args.outfile + "_" + str(i)
-			encodeText(readFileSize(args.infile, size, i*size, args.verbose), str(args.outfile+"_"+str(i)), args.img_width, args.img_height, imageType, args.verbose)
+			encodeText(readFileSize(args.infile, size, i*size, args.verbose), str(args.outfile+"_"+str(i)), args.img_width, args.img_height, imageType, args.verbose, args.encrypt, args.key)
 		quit(0)
 	
 	if args.encode:
 		if args.verbose:
 			print "[DEBUG] Input File Name: " + str(args.infile)
 			print "[DEBUG] Output File Name: " + str(args.outfile)
-		encode(args.infile, args.outfile, args.img_width, args.img_height, imageType, args.verbose)
+		encode(args.infile, args.outfile, args.img_width, args.img_height, imageType, args.verbose, args.encrypt, args.key)
 		quit(0)
 	elif args.decode:
 		if args.verbose:
 			print "[DEBUG] Input File Name: " + str(args.infile)
 			print "[DEBUG] Output File Name: " + str(args.outfile)
-		decode(args.infile, args.outfile, imageType, args.verbose)
+		decode(args.infile, args.outfile, imageType, args.verbose, args.decrypt, args.key)
 		quit(0)
 	else:
 		print "[!!] Not sure what to do, specify encode or decode!"
